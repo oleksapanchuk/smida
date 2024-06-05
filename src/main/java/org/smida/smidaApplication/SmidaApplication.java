1219a30aeb49bd5b201c5d0bc2e0b2476548bf03
Oleksandr Panchuk <olexandrpan4uk@gmail.com>
2024-06-05 13:06:29 +0300

    init

---
+++ src/main/java/org/smida/smidaApplication/SmidaApplication.java
@@ -0,0 +1,13 @@
+package org.smida.smidaApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/smida/SmidaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
